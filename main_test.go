075bbf7c40bdd302b0030a8bf1277e83e07efdc2
Aldjinn <aldjinn@gmail.com>
2025-05-17 22:31:01 +0200

    Refactor CI workflow and configuration files for consistency and formatting

--- main_test.go
+++ main_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-    want := "Hello, world!"
-    got := getHelloWorld()
-    if got != want {
-        t.Errorf("got %q, want %q", got, want)
-    }
+	want := "Hello, world!"
+	got := getHelloWorld()
+	if got != want {
+			t.Errorf("got %q, want %q", got, want)
+	}
 }
